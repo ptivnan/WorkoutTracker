43b6c80ab18d858d3fecc85f2e0e42bdcfd7b02e
Patrick Tivnan <ptivnan@gmail.com>
2021-11-04 20:56:19 -0400

    Sets now update via AJAX

--- static/js/workout_tracker.js
+++ static/js/workout_tracker.js
@@ -11,6 +11,28 @@ $(document).ready(function() {
         $("#form_workout_update").submit()
 
     })
+    $("input[id*='id_set_set-']").focusout(function() {
+        console.log("Focus out!")
+        frm = $(this).closest("form")
+        token = $(this).closest("[name='csrfmiddlewaretoken'")
+        frm.submit(function () {
+            console.log("Submit!")
+            $.ajax({
+                type: frm.attr('method'),
+                url: frm.attr('action'),
+                data: frm.serialize(),
+                header: {'X-CSRFToken':token},
+                success: function (data) {
+                    console.log(data);
+                },
+                error: function(data) {
+                    console.log("Something went wrong!");
+                }
+            });
+            return false;
+        });
+        frm.submit()
+    })
 })
 
 // $('#fail-btn').each(function () {
